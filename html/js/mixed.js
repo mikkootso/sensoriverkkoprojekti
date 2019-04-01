d7a7d6db9552551b4fcd8b57741ea45435d124dd
mikkootso <38580569+mikkootso@users.noreply.github.com>
2019-04-01 18:31:51 +0300

    v2

--- html/js/mixed.js
+++ html/js/mixed.js
@@ -12,19 +12,19 @@ new Chart(document.getElementById("mixed"), {
           label: "Valoisuus",
           type: "line",
           borderColor: "#3e95cd",
-          data: [133,221,783,578],
+          data: [133,221,783,578, 408,547,675,734,408,547,675,473],
           fill: false
         }, {
           label: "PH",
           type: "bar",
           backgroundColor: "rgba(0,30,0,0.2)",
-          data: [408,547,675,734],
+          data: [408,547,675,734,432,562,198,124,754,323,382,432],
         }, {
           label: "Kosteus",
           type: "bar",
           backgroundColor: "rgba(120,0,40,0.2)",
           backgroundColorHover: "#3e95cd",
-          data: [133,221,783,978]
+          data: [133,221,783,978,221,325,111,231,553,534,651,123]
         }
       ]
     },
@@ -33,6 +33,6 @@ new Chart(document.getElementById("mixed"), {
         display: true,
         text: 'Vuoden tilastot'
       },
-      legend: { display: false }
+      legend: { display: true }
     }
 });
