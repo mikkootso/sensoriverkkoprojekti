a782653e239a7125474d8e0991d2ebf2ef95b06c
Heikki <heikkimat@gmail.com>
2019-04-04 20:09:30 +0300

    New select command

--- html/add.php
+++ html/add.php
@@ -5,9 +5,12 @@
 
 	$temp1=$_POST["temp1"];
 	$hum1=$_POST["hum1"];
+	$ldr=$_POST["ldr"];
+	$co2=$_POST["co2"];
+	$temp2=$_POST["temp2"];
 	
-	$query = "INSERT INTO `tempHum` (`temperature`, `humidity`) 
-		VALUES ('".$temp1."','".$hum1."')"; 
+	$query = "INSERT INTO `grow` (`temp1`, `hum1`, `ldr`, `co2`, `temp2`) 
+		VALUES ('".$temp1."','".$hum1."','".$ldr."','".$co2."','".$temp2."')"; 
 	
 	if ($result = $link->query($query))
 		{ echo json_encode("New record created successfully");
