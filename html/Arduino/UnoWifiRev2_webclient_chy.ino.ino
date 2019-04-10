483e78ea5f27db106fd4504687801d9cce6273bf
Heikki <heikkimat@gmail.com>
2019-04-10 14:58:11 +0300

    4 sensoria toiminnassa, mittaukset tulostuvat korteihin ja viivakaavioihin

--- html/Arduino/UnoWifiRev2_webclient_chy.ino.ino
+++ html/Arduino/UnoWifiRev2_webclient_chy.ino.ino
@@ -1,22 +1,28 @@
-// HEikki Mattila 050419
+// HEikki Mattila 100419
 // Sending sensor data to Chydenius server
 // using Uno WiFi Rev2 board and Post method. 
 // Add.php handles data at the server side.
+// Sensors:
+//        DHT22 - temperature and humidity
+//        TEMT6000 - light intensity
+//        Gravity infrared CO2 sensor v1.1
+
 
 #include <DHT.h>
 #include <SPI.h>
 #include <WiFiNINA.h>
 
-#define DHTPIN 2 // SENSOR PIN
+#define DHTPIN 2 // temperature and humidity sensor pin
 #define DHTTYPE DHT22 // SENSOR TYPE - THE ADAFRUIT LIBRARY OFFERS SUPPORT FOR MORE MODELS
+#define CO2PIN A1 // co2 sensor pin
+#define LDRPIN A2 // light intensity sensor pin
+
 DHT dht(DHTPIN, DHTTYPE); //Initialize DHT sensor
 
 #include "arduino_secrets.h" 
 ///////please enter your sensitive data in the Secret tab/arduino_secrets.h
 char ssid[] = SECRET_SSID;        // your network SSID (name)
 char pass[] = SECRET_PASS;    // your network password (use for WPA, or use as key for WEP)
-int keyIndex = 0;            // your network key Index number (needed only for WEP)
-
 int status = WL_IDLE_STATUS;
 
 // Initialize the Wifi client library
@@ -24,7 +30,6 @@ WiFiClient client;
 
 // server address:
 char server[] = "luna.chydenius.fi";
-//IPAddress server(64,131,82,241);
 
 unsigned long lastConnectionTime = 0;            // last time you connected to the server, in milliseconds
 const unsigned long postingInterval = 300L * 1000L; // delay between updates, in milliseconds
@@ -69,9 +74,9 @@ void setup() {
   printWifiStatus();
 
   dht.begin(); 
-  delay(10000); // GIVE THE SENSOR SOME TIME TO START
-  h1 = (int) dht.readHumidity(); 
-  t1 = (int) dht.readTemperature(); 
+  //delay(10000); // GIVE THE SENSOR SOME TIME TO START
+  //h1 = (int) dht.readHumidity(); 
+  //t1 = (int) dht.readTemperature(); 
   //Serial.println(h1);
   //Serial.println(t1);
 
@@ -92,9 +97,9 @@ void loop() {
   if (millis() - lastConnectionTime > postingInterval) {
     h1 = (int) dht.readHumidity();
     t1 = (int) dht.readTemperature();
-    ldr = 7000 + random(31); //lux
-    co2 = 1000 + random(21); //ppm
-    t2 = 22 + random(2); //C
+    ldr = (int) lightIntensity(); //lux
+    co2 = (int) co2Concentration(); //ppm
+    t2 = 22 + random(3); //C  dummy data
     
     httpRequest();
   }
@@ -134,7 +139,7 @@ void httpRequest() {
   }
 }
 
-
+// Prints out the WiFi status
 void printWifiStatus() {
   // print the SSID of the network you're attached to:
   Serial.print("SSID: ");
@@ -150,4 +155,43 @@ void printWifiStatus() {
   Serial.print("signal strength (RSSI):");
   Serial.print(rssi);
   Serial.println(" dBm");
+}
+
+//function returns co2 sensor consentration value
+float co2Concentration(){
+  int sensorValue = analogRead(CO2PIN);  
+  float voltage = sensorValue*(5000/1024.0); 
+  if(voltage == 0){
+    Serial.println("CO2 sensor fault)");
+    return 0.0;
+  }
+  else if(voltage < 400){
+    Serial.println("CO2 sensor preheating");
+    return 0.0;
+  }
+  else{
+    // measure n times and return the average
+    int n = 100;
+    float concentration = 0;
+    for(int i=0; i<n; i++){
+      sensorValue = analogRead(CO2PIN);
+      voltage = sensorValue*(5000/1024.0); 
+      concentration += (voltage-400.0)*50.0/16.0;
+      delay(100); 
+    }
+    return concentration/n;
+  }
+}
+
+// function return light intensity in LUX
+float lightIntensity(){
+  int n = 100;
+  int val;
+  float lux = 0;
+  for(int i=0; i<n; i++){
+    val = analogRead(LDRPIN);
+    lux += val*1000.0/1023.0;
+    delay(100);
+  }
+  return lux/n;
 }
--- html/js/linegraph.js
+++ html/js/linegraph.js
@@ -12,6 +12,16 @@ $(document).ready(function(){
         var ldr = [];
         var co2 = [];
         var temp2 = [];
+
+        // Min and max limits
+        var temp1Min = 20;
+        var temp1Max = 30;
+        var hum1Min = 30;
+        var hum1Max = 80;
+        var ldrMin = 100;
+        var ldrMax = 900;
+        var co2Min = 400;
+        var co2Max = 1200;
         
   
         for(var i in data) {
@@ -30,6 +40,7 @@ $(document).ready(function(){
           datasets: [
             {
               label: "Ilman lämpötila",
+              yAxisID: 'A',
               fill: false,
               lineTension: 0.1,
               backgroundColor: "rgba(59, 89, 152, 0.75)",
@@ -40,6 +51,7 @@ $(document).ready(function(){
             },
             {
               label: "Ilman kosteus",
+              yAxisID: 'A',
               fill: false,
               lineTension: 0.1,
               backgroundColor: "rgba(29, 202, 255, 0.75)",
@@ -50,6 +62,7 @@ $(document).ready(function(){
             },
             {
               label: "Valoisuus",
+              yAxisID: 'B',
               fill: false,
               lineTension: 0.1,
               backgroundColor: "rgba(229, 202, 25, 0.75)",
@@ -60,6 +73,7 @@ $(document).ready(function(){
             },
             {
               label: "Hiilidioksidi",
+              yAxisID: 'B',
               fill: false,
               lineTension: 0.1,
               backgroundColor: "rgba(0, 255, 0, 0.75)",
@@ -70,6 +84,7 @@ $(document).ready(function(){
             },
             {
               label: "Veden lämpötila",
+              yAxisID: 'A',
               fill: false,
               lineTension: 0.1,
               backgroundColor: "rgba(255, 0, 0, 0.75)",
@@ -84,7 +99,28 @@ $(document).ready(function(){
         var ctx = $("#mixed");
         var LineGraph = new Chart(ctx, {
           type: 'line',
-          data: chartdata
+          data: chartdata,
+          options: {
+            scales: {
+              yAxes: [{
+                id: 'A',
+                type: 'linear',
+                position: 'left',
+                ticks: {
+                  max: 100,
+                  min: 0
+                }
+              }, {
+                id: 'B',
+                type: 'linear',
+                position: 'right',
+                ticks: {
+                  max: 1000,
+                  min: 0
+                }
+              }]
+            }
+          }
         });
 
         //chart 2 
@@ -173,61 +209,113 @@ $(document).ready(function(){
         ]
       };
 
-    var htx = $("#co2");
-    var LineGraph3 = new Chart(htx, {
-      type: 'line',
-      data: co2data,
-      options: {
-        scales: {
-          yAxes: [{
-            ticks: {
-              beginAtZero: false,
-              min: 300,
-              max: 1300,
-              stepSize: 50,
-            }
-          }]
-        }
-      }
-    }); //
-
-    //chart 6 
-    var temp2data = {
-      labels: tstamp,
-      datasets: [
-        {
-          label: "Veden lämpötila",
-          fill: false,
-          lineTension: 0.1,
-          backgroundColor: "rgba(255, 0, 0, 0.75)",
-          borderColor: "rgba(255, 0, 0, 1)",
-          pointHoverBackgroundColor: "rgba(255, 0, 0, 1)",
-          pointHoverBorderColor: "rgba(255, 0, 0, 1)",
-          data: temp2
-        }
-      ]
-    };
-
-  var htx = $("#temp2");
-  var LineGraph3 = new Chart(htx, {
-    type: 'line',
-    data: temp2data,
-    options: {
-      scales: {
-        yAxes: [{
-          ticks: {
-            beginAtZero: true,
-            min: 0,
-            max: 30,
-            stepSize: 2,
+      var htx = $("#co2");
+      var LineGraph3 = new Chart(htx, {
+        type: 'line',
+        data: co2data,
+        options: {
+          scales: {
+            yAxes: [{
+              ticks: {
+                beginAtZero: false,
+                min: 300,
+                max: 1300,
+                stepSize: 50,
+              }
+            }]
           }
-        }]
+        }
+      }); //
+
+      //chart 6 
+      var temp2data = {
+        labels: tstamp,
+        datasets: [
+          {
+            label: "Veden lämpötila",
+            fill: false,
+            lineTension: 0.1,
+            backgroundColor: "rgba(255, 0, 0, 0.75)",
+            borderColor: "rgba(255, 0, 0, 1)",
+            pointHoverBackgroundColor: "rgba(255, 0, 0, 1)",
+            pointHoverBorderColor: "rgba(255, 0, 0, 1)",
+            data: temp2
+          }
+        ]
+      };
+
+      /*
+      var htx = $("#temp2");
+      var LineGraph3 = new Chart(htx, {
+        type: 'line',
+        data: temp2data,
+        options: {
+          scales: {
+            yAxes: [{
+              ticks: {
+                beginAtZero: true,
+                min: 0,
+                max: 30,
+                stepSize: 2,
+              }
+            }]
+          }
+        }
+      }); 
+      */
+      
+      //  temp1Card
+      let tv = document.getElementById("tValue");
+      tv.innerHTML = data[data.length-1].temp1 + " C";
+      let tt = document.getElementById("tTime");
+      tt.innerHTML = "Viimeksi päivitetty " + data[data.length-1].timeStamp;
+      if (data[data.length-1].temp1 < temp1Min || data[data.length-1].temp1 > temp1Max) {
+        let tb = document.getElementById("tBorder");
+        tb.setAttribute("class", "card border-danger text-center");
+        let tt = document.getElementById("tText");
+        tt.setAttribute("class", "card-body text-danger");
       }
+      //  hum1Card
+      let hv = document.getElementById("hValue");
+      hv.innerHTML = data[data.length-1].hum1 + " %";
+      let ht = document.getElementById("hTime");
+      ht.innerHTML = "Viimeksi päivitetty " + data[data.length-1].timeStamp;
+      if (data[data.length-1].hum1 < hum1Min || data[data.length-1].hum1 > hum1Max) {
+        let hb = document.getElementById("hBorder");
+        hb.setAttribute("class", "card border-danger text-center");
+        let ht = document.getElementById("hText");
+        ht.setAttribute("class", "card-body text-danger");
+      }
+      //  ldrCard
+      let lv = document.getElementById("lValue");
+      lv.innerHTML = data[data.length-1].ldr + " lx";
+      let lt = document.getElementById("lTime");
+      lt.innerHTML = "Viimeksi päivitetty " + data[data.length-1].timeStamp;
+      if (data[data.length-1].ldr < ldrMin || data[data.length-1].ldr > ldrMax) {
+        let lb = document.getElementById("lBorder");
+        lb.setAttribute("class", "card border-danger text-center");
+        let lt = document.getElementById("lText");
+        lt.setAttribute("class", "card-body text-danger");
+      }
+      //  co2Card
+      let cv = document.getElementById("cValue");
+      cv.innerHTML = data[data.length-1].co2 + " ppm";
+      let ct = document.getElementById("cTime");
+      ct.innerHTML = "Viimeksi päivitetty " + data[data.length-1].timeStamp;
+      if (data[data.length-1].co2 < co2Min || data[data.length-1].co2 > co2Max) {
+        let cb = document.getElementById("cBorder");
+        cb.setAttribute("class", "card border-danger text-center");
+        let ct = document.getElementById("cText");
+        ct.setAttribute("class", "card-body text-danger");
+      }
+
+    },
+    error : function(data) {
+
     }
-  }); //
-      },
-      error : function(data) {
-  
-      }
-    });
   });
+
+  
+});
+  
+  
